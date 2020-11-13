12fcfcbaf5cbe851c4820deeed54f47e2b3b3430
licong <licong@qianxin.com>
2020-11-13 09:16:15 +0000

    add github proxy

--- main.go
+++ main.go
@@ -8,11 +8,19 @@ import (
 
 var output string
 var url string
+var proxy bool
+var github_proxy = "https://g.ioiox.com/"
 
 func init() {
 	flag.StringVar(&output, "o", "", "output filename")
 	flag.StringVar(&url, "url", "", "url")
+	flag.BoolVar(&proxy, "proxy", false, "github proxy default: https://g.ioiox.com/ ")
+	flag.StringVar(&github_proxy, "github-proxy", "https://g.ioiox.com/", "github proxy server")
 	flag.Parse()
+	if proxy {
+		fmt.Println("donwloading by github proxy: https://g.ioiox.com/ ")
+		url = github_proxy + url
+	}
 }
 func main() {
 	client := network.NewFile(url)
