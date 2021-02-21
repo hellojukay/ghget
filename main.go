b49bf56211e96b90c3cd00ab0aa7c3e38ac3a736
hellojukay <hellojukay@163.com>
2021-02-21 14:36:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,20 +11,20 @@ import (
 var output string
 var url string
 var proxy bool
-var github_proxy = "https://g.ioiox.com/"
+var github_proxy = "https://ghproxy.com/"
 
 func init() {
 	flag.StringVar(&output, "o", "", "output filename")
 	flag.StringVar(&url, "url", "", "url")
-	flag.BoolVar(&proxy, "proxy", false, "github proxy default: https://g.ioiox.com/ ")
-	flag.StringVar(&github_proxy, "github-proxy", "https://g.ioiox.com/", "github proxy server")
+	flag.BoolVar(&proxy, "proxy", false, "github proxy default: https://ghproxy.com/ ")
+	flag.StringVar(&github_proxy, "github-proxy", "https://ghproxy.com/", "github proxy server")
 	flag.Parse()
 	if url == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 	if proxy {
-		fmt.Println("donwloading by github proxy: https://g.ioiox.com/ ")
+		fmt.Println("donwloading by github proxy: https://ghproxy.com/ ")
 		url = github_proxy + url
 	}
 }
